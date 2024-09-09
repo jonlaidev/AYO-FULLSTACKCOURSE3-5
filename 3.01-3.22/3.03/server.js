f0b414711f55150778d9d7e85e0c7f83721bbc5b
Joni Laine <164735249+jonlaidev@users.noreply.github.com>
2024-09-09 19:24:16 +0300

    3.01-3.03

---
+++ 3.01-3.22/3.03/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
+  }
+];
+
+// get_persons
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+// get_person
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).send({ error: 'Person not found' });
+  }
+});
+
+//get_info
+app.get('/info', (req, res) => {
+  const numberOfPersons = persons.length;
+  const currentDate = new Date();
+
+  res.send(`
+    <p>Phonebook has info for ${numberOfPersons} people</p>
+    <p>${currentDate}</p>
+  `);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
